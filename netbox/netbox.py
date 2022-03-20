2c7529831cb5e5c09195cb391313903b6c244f30
mi2428 <mi2428782020@gmail.com>
2022-03-21 01:00:19 +0900

    keep: Mon Mar 21 01:00:19 JST 2022

--- netbox/netbox.py
+++ netbox/netbox.py
@@ -16,79 +16,82 @@ VAULT_PASSWORD_FILE = os.path.join(os.path.dirname(__file__), "../.secrets/vault
 
 
 def load_encrypted_secrets():
-  with open(VAULT_FILE) as v, open(VAULT_PASSWORD_FILE, "r") as p:
-    key = str.encode(p.read().rstrip())
-    try:
-      vault = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(key))])
-      raw = vault.decrypt(v.read())
-      return yaml.load(raw, Loader=yaml.CLoader)
-    except AnsibleVaultError as e:
-      print("Failed to decrypt the vault. Check your password and try again:", e, file=sys.stderr)
-      sys.exit(1)
+    with open(VAULT_FILE) as v, open(VAULT_PASSWORD_FILE, "r") as p:
+        key = str.encode(p.read().rstrip())
+        try:
+            vault = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(key))])
+            raw = vault.decrypt(v.read())
+            return yaml.load(raw, Loader=yaml.CLoader)
+        except AnsibleVaultError as e:
+            print("Failed to decrypt the vault. Check your password and try again:", e, file=sys.stderr)
+            sys.exit(1)
 
 
 class NetBoxClient:
-  def __init__(self, netbox_url, netbox_api_token):
-    self.netbox_url = netbox_url
-    self.api_endpoint = netbox_url.rstrip("/") + "/api"
-    self.token = netbox_api_token
-    self.all_sites = []
-    self.all_vlans = []
-    self.all_devices = []
-    self.all_interfaces = []
-    self.all_addresses = []
+    def __init__(self, netbox_url, netbox_api_token):
+        self.netbox_url = netbox_url
+        self.api_endpoint = netbox_url.rstrip("/") + "/api"
+        self.token = netbox_api_token
+        self.all_sites = []
+        self.all_vlans = []
+        self.all_devices = []
+        self.all_interfaces = {}
+        self.all_addresses = []
 
 
-  def query(self, request_path):
-    responses = []
-    url = self.api_endpoint + request_path
-    headers = {
-      "Authorization": f"Token {self.token}",
-      "Content-Type":  "application/json",
-      "Accept":        "application/json; indent=4"
-    }
+    def query(self, request_path):
+        responses = []
+        url = self.api_endpoint + request_path
+        headers = {
+            "Authorization": f"Token {self.token}",
+            "Content-Type":  "application/json",
+            "Accept":                "application/json; indent=4"
+        }
 
-    while url:
-      raw = requests.get(url, headers=headers, verify=True)
-      res = json.loads(raw.text)
-      responses += res["results"]
-      url = res["next"]
-    return responses
-
-
-  def get_all_sites(self, use_cache=True):
-    if not use_cache or not self.all_sites:
-      self.all_sites = self.query("/dcim/sites/")
-    return self.all_sites
+        while url:
+            raw = requests.get(url, headers=headers, verify=True)
+            res = json.loads(raw.text)
+            responses += res["results"]
+            url = res["next"]
+        return responses
 
 
-  def get_all_vlans(self, use_cache=True):
-    if not use_cache or not self.all_vlans:
-      self.all_vlans = self.query("/ipam/vlans/")
-      for vlan in self.all_vlans:
-        vlan["tags"] = [tag["slug"] for tag in vlan["tags"]]
-    return self.all_vlans
+    def get_all_sites(self, use_cache=True):
+        if not use_cache or not self.all_sites:
+            self.all_sites = self.query("/dcim/sites/")
+        return self.all_sites
 
 
-  def get_all_devices(self, use_cache=True):
-    if not use_cache or not self.all_devices:
-      self.all_devices = self.query("/dcim/devices/")
-      for device in self.all_devices:
-        device["tags"] = [tag["slug"] for tag in device["tags"]]
-    return self.all_devices
+    def get_all_vlans(self, use_cache=True):
+        if not use_cache or not self.all_vlans:
+            self.all_vlans = self.query("/ipam/vlans/")
+            for vlan in self.all_vlans:
+                vlan["tags"] = [tag["slug"] for tag in vlan["tags"]]
+        return self.all_vlans
 
 
-  def get_all_interfaces(self, use_cache=True):
-    if not use_cache or not self.all_interfaces:
-      self.all_interfaces = self.query("/dcim/interfaces/")
-      for interface in self.all_interfaces:
-        interface["tags"] = [tag["slug"] for tag in interface["tags"]]
-    return self.all_interfaces
+    def get_all_devices(self, use_cache=True):
+        if not use_cache or not self.all_devices:
+            self.all_devices = self.query("/dcim/devices/")
+            for device in self.all_devices:
+                device["tags"] = [tag["slug"] for tag in device["tags"]]
+        return self.all_devices
 
 
-  def get_all_addresses(self, use_cache=True):
-    if not use_cache or not self.all_addresses:
-      self.all_addresses = self.query("/ipam/ip-addresses/")
-      for address in self.all_addresses:
-        address["tags"] = [tag["slug"] for tag in address["tags"]]
-    return self.all_addresses
+    def get_all_interfaces(self, use_cache=True):
+        if not use_cache or not self.all_interfaces:
+            all_interfaces = self.query("/dcim/interfaces/")
+            for interface in all_interfaces:
+                interface["tags"] = [tag["slug"] for tag in interface["tags"]]
+                dev_name = interface["device"]["name"]
+                int_name = interface["name"]
+                self.all_interfaces.setdefault(dev_name, {})[int_name] = interface
+        return self.all_interfaces
+
+
+    def get_all_addresses(self, use_cache=True):
+        if not use_cache or not self.all_addresses:
+            self.all_addresses = self.query("/ipam/ip-addresses/")
+            for address in self.all_addresses:
+                address["tags"] = [tag["slug"] for tag in address["tags"]]
+        return self.all_addresses
