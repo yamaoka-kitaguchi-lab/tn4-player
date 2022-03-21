77f7f54712bd7a62c512b385072c5223ab722674
mi2428 <mi2428782020@gmail.com>
2022-03-21 23:41:02 +0900

    keep: Mon Mar 21 23:41:02 JST 2022

--- netbox/netbox.py
+++ netbox/netbox.py
@@ -149,7 +149,7 @@ class NetBoxClient:
                     if Slug.tag_vlan_suzukake in vlan["tags"]:
                         self.wifi_vlanids_s.append(vlan["id"])
 
-                self.all_vlans[vlan["id"]] = vlan
+                self.all_vlans[str(vlan["id"])] = vlan
         return self.all_vlans
 
 
@@ -251,3 +251,37 @@ class NetBoxClient:
 
                 self.all_interfaces.setdefault(dev_name, {})[int_name] = interface
         return self.all_interfaces
+
+
+    def update_interface(self, device_name, interface_name,
+                         description=None, enabled=None, mode=None, untagged_vlanid=None, tagged_vlanids=None, tags=None):
+        data = []
+        body = {
+            "id": self.all_interfaces[device_name][interface_name]["id"]
+        }
+
+        if description is not None:
+            body["description"] = description
+
+        if enabled is not None:
+            body["enabled"] = enabled
+
+        if tags is not None:
+            body["tags"] = [{"slug": tag} for tag in tags]
+
+        if mode is not None:
+            if mode.lower() == "access":
+                body["mode"] = "access"
+            if mode.lower() in ["tagged", "trunk"]:
+                body["mode"] = "tagged"
+
+        if untagged_vlanid is not None:
+            body["untagged_vlan"] = int(untagged_vlanid)
+
+        if tagged_vlanids is not None:
+            body["tagged_vlans"] = list(map(int, tagged_vlanids))
+
+        data.append(body)
+        if data:
+            return self.query("/dcim/interfaces/", data, update=True)
+        return
