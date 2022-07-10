b8b4c8583d2bb6710d979affbf7cdcbd8c71abb8
mi2428 <mi2428782020@gmail.com>
2022-07-11 00:33:40 +0900

    keep: Mon Jul 11 00:33:40 JST 2022

--- lib/tn4/netbox/addresses.py
+++ lib/tn4/netbox/addresses.py
@@ -1,8 +1,16 @@
+from tn4.netbox.base import ClientBase
 
 
-    def get_all_addresses(self, use_cache=True):
-        if not use_cache or not self.all_addresses:
-            self.all_addresses = self.query("/ipam/ip-addresses/")
-            for address in self.all_addresses:
-                address["tags"] = [tag["slug"] for tag in address["tags"]]
-        return self.all_addresses
+class Addresses(ClientBase):
+    path = '/ipam/ip-addresses/'
+
+    def __init___(self):
+        super().__init___()
+
+    def get_addresses(self, ctx):
+        all_addresses = self.query(ctx, path)
+        for address in all_addresses:
+            address["tags"] = [tag["slug"] for tag in address["tags"]]
+
+        return all_addresses
+
--- lib/tn4/netbox/vlans.py
+++ lib/tn4/netbox/vlans.py
@@ -1,28 +1,20 @@
-    def get_all_vlans(self, use_cache=True):
-        if not use_cache or not self.all_vlans:
-            all_vlans = self.query("/ipam/vlans/")
-            for vlan in all_vlans:
-                vlan["tags"] = [tag["slug"] for tag in vlan["tags"]]
-                if Slug.tag_mgmt_vlan_edge_ookayama in vlan["tags"]:
-                    self.mgmt_vlanid_eo = vlan["id"]
-                if Slug.tag_mgmt_vlan_edge_suzukake in vlan["tags"]:
-                    self.mgmt_vlanid_es = vlan["id"]
-                if Slug.tag_mgmt_vlan_core_ookayama in vlan["tags"]:
-                    self.mgmt_vlanid_co = vlan["id"]
-                if Slug.tag_mgmt_vlan_core_suzukake in vlan["tags"]:
-                    self.mgmt_vlanid_cs = vlan["id"]
+from tn4.netbox.base import ClientBase
+from tn4.netbox.slug import Slug
 
-                if Slug.tag_wifi_mgmt_vlan_ookayama1 in vlan["tags"]:
-                    self.wifi_mgmt_vlanid_o1 = vlan["id"]
-                if Slug.tag_wifi_mgmt_vlan_ookayama2 in vlan["tags"]:
-                    self.wifi_mgmt_vlanid_o2 = vlan["id"]
-                if Slug.tag_wifi_mgmt_vlan_suzukake in vlan["tags"]:
-                    self.wifi_mgmt_vlanid_s = vlan["id"]
-                if Slug.tag_wifi in vlan["tags"] and vlan["status"]["value"] == "active":
-                    if Slug.tag_vlan_ookayama in vlan["tags"]:
-                        self.wifi_vlanids_o.append(vlan["id"])
-                    if Slug.tag_vlan_suzukake in vlan["tags"]:
-                        self.wifi_vlanids_s.append(vlan["id"])
 
-                self.all_vlans[str(vlan["id"])] = vlan
+class Vlans(ClientBase):
+    path = '/ipam/vlans/'
+
+    def __init__(self):
+        super().__init__()
+        self.all_vlans = {}
+
+    def get_vlans(self, ctx):
+        all_vlans = self.query(ctx, path)
+
+        for vlan in all_vlans:
+            vlan["tags"] = [tag["slug"] for tag in vlan["tags"]]
+            self.all_vlans[str(vlan["id"])] = vlan
+
         return self.all_vlans
+
