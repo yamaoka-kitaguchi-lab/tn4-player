85b26b711a4c90b2db7ea8267df4642249704465
mi2428 <mi2428782020@gmail.com>
2022-10-03 22:54:49 +0900

    keep: Mon Oct  3 22:54:49 JST 2022

---
+++ lib/tn4/cli/netbox.py
@@ -0,0 +1,11 @@
+#from tn4.helper.utils import inventory_filter
+
+
+class NetBox:
+    def __init__(self, args):
+        print(args)
+        print(args.no_hosts)
+
+
+    def exec(self, stdout=False):
+        pass
---
+++ lib/tn4/cli/shutdown.py
@@ -0,0 +1,11 @@
+#from tn4.helper.utils import inventory_filter
+
+
+class Shutdown:
+    def __init__(self, args):
+        print(args)
+        print(args.no_hosts)
+
+
+    def exec(self, stdout=False):
+        pass
