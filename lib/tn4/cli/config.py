7785cacba868eb8af4c1505ed955c2b48c4ab8c3
mi2428 <mi2428782020@gmail.com>
2022-10-04 00:10:37 +0900

    keep: Tue Oct  4 00:10:37 JST 2022

--- lib/tn4/cli/config.py
+++ lib/tn4/cli/config.py
@@ -1,11 +1,14 @@
-#from tn4.helper.utils import inventory_filter
+from tn4.cli.base import CommandBase
 
 
-class Config:
-    def __init__(self, args):
-        print(args)
-        print(args.no_hosts)
+class Config(CommandBase):
+    def __init__(self, args, use_cache=False):
+        self.use_cache=use_cache
+        self.fetch_inventory(hosts=args.hosts, no_hosts=args.no_hosts,
+                             areas=args.areas, no_areas=args.no_areas,
+                             roles=args.roles, no_roles=args.no_roles, use_cache=self.use_cache)
 
 
     def exec(self, stdout=False):
-        pass
+        print(self.inventory["_meta"]["hostvars"].keys())
+
