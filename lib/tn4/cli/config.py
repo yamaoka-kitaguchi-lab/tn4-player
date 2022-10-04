29432413c8c4959a9228e7323067c8b56ad38fbb
mi2428 <mi2428782020@gmail.com>
2022-10-04 23:22:26 +0900

    keep: Tue Oct  4 23:22:26 JST 2022

--- lib/tn4/cli/config.py
+++ lib/tn4/cli/config.py
@@ -8,7 +8,8 @@ from tn4.cli.base import CommandBase
 
 class Config(CommandBase):
     def __init__(self, args):
-        self.use_cache=args.use_cache
+        self.flg_use_cache = args.use_cache
+        self.flg_inventory = args.inventory
         self.fetch_inventory_opt = [
             args.hosts, args.no_hosts, args.areas, args.no_areas, args.roles, args.no_roles,
             args.vendors, args.no_vendors, args.tags, args.no_tags, args.use_cache
