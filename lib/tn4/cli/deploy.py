2784a276b9eda2ee1fce40cde5447f1d4388963b
mi2428 <mi2428782020@gmail.com>
2022-10-15 20:21:59 +0900

    keep: Sat Oct 15 20:21:59 JST 2022

--- lib/tn4/cli/deploy.py
+++ lib/tn4/cli/deploy.py
@@ -10,7 +10,7 @@ class Deploy(CommandBase):
         self.flg_dryrun           = args.dryrun
         self.flg_debug            = args.debug
         self.custom_template_path = args.template
-        self.fetch_inventory_opt = [
+        self.fetch_inventory_opt  = [
             args.hosts, args.no_hosts, args.areas, args.no_areas, args.roles, args.no_roles,
             args.vendors, args.no_vendors, args.tags, args.no_tags, args.use_cache
         ]
