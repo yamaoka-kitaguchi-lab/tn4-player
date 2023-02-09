6a44c661bd3fdc2cea2fb7e95aedc997b3b17dd7
mi2428 <mi2428782020@gmail.com>
2023-02-09 16:54:35 +0900

    keep: Thu Feb  9 04:54:35 PM JST 2023

--- lib/tn4/nbck/state.py
+++ lib/tn4/nbck/state.py
@@ -7,3 +7,19 @@ class InterfaceState:
     untagged_vlanid     = None  # NetBox object IDs
     tagged_vids         = None  # 802.1q VLAN IDs
     untagged_vid        = None
+
+    def __init__(self, nb_interface_obj=None):
+        self.is_enabled          = nb_interface_obj["enabled"]
+        self.description         = nb_interface_obj["description"]
+        self.tags                = nb_interface_obj["tags"]
+
+        try:
+            self.is_tagged_vlan_mode = nb_interface_obj["mode"]["value"] == "tagged"
+        except KeyError:
+            self.is_tagged_vlan_mode = False
+
+        self.tagged_vlanids  = nb_interface_obj["tagged_vlanids"]
+        self.untagged_vlanid = nb_interface_obj["untagged_vlanid"]
+        self.tagged_vids  = nb_interface_obj["tagged_vids"]
+        self.untagged_vid = nb_interface_obj["untagged_vid"]
+
