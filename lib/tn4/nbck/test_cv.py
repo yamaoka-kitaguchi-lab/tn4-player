e23b654e16977b7ab40d703bade3075dd282f9ed
mi2428 <mi2428782020@gmail.com>
2023-03-08 23:14:35 +0900

    keep: Wed Mar  8 23:14:35 JST 2023

--- lib/tn4/nbck/test_cv.py
+++ lib/tn4/nbck/test_cv.py
@@ -16,6 +16,12 @@ class TestCV(unittest.TestCase):
         x = CV({1,2,3}, Cond.IS)
         self.assertTrue(is_equal(a+b, x))
 
+    def test_add_is_to_include(self):
+        a = CV({1,3}, Cond.IS)
+        b = CV({1,2,3}, Cond.INCLUDE)
+        x = CV({1,3}, Cond.IS)
+        self.assertTrue(is_equal(a+b, x))
+
 
 if __name__ == "__main__":
     unittest.main()
