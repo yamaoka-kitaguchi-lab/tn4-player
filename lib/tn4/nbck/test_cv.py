fbd604379187c6a49d55e43200a43117d5ed9830
mi2428 <mi2428782020@gmail.com>
2023-03-08 23:11:03 +0900

    keep: Wed Mar  8 23:11:03 JST 2023

--- lib/tn4/nbck/test_cv.py
+++ lib/tn4/nbck/test_cv.py
@@ -1,5 +1,21 @@
+import unittest
+
 from cv import Condition as Cond
 from cv import ConditionalValue as CV
 
 
+def is_equal(a, b):
+    return a.value == b.value and a.condition == b.condition
 
+
+class TestCV(unittest.TestCase):
+
+    def test_add_is_to_is(self):
+        a = CV({1,2,3}, Cond.IS)
+        b = CV({1,2,3}, Cond.IS)
+        x = CV({1,2,3}, Cond.IS)
+        self.assertTrue(is_equal(a+b, x))
+
+
+if __name__ == "__main__":
+    unittest.main()
