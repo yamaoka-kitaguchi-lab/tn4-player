e3b664330c0b036bbf5cebc7a08c1c52ced391f7
mi2428 <mi2428782020@gmail.com>
2023-03-18 14:03:42 +0900

    keep: Sat Mar 18 14:03:42 JST 2023

--- lib/tn4/doctor/diagnose.py
+++ lib/tn4/doctor/diagnose.py
@@ -154,6 +154,9 @@ class Diagnose(Base):
         wifi_s_dplane_vids = self.nb_vlans.with_tags(Slug.Tag.Wifi, Slug.Tag.VlanSuzukake).vids
 
         for hostname, device_interfaces in self.nb_interfaces.all.items():
+            if hostname not in self.nb_devices.all:
+                continue
+
             device = self.nb_devices.all[hostname]
 
             cplane_vid, dplane_vids = None, None
